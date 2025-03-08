5806060d7a6cd1dc6015afed1dbcb4a206161750
edsel <edsel.tan22@gmail.com>
2025-03-08 12:37:41 +0800

    Updated datacleaning.py to have a class object

--- Subgroup-B-questions/Question 3/datacleaning.py
+++ Subgroup-B-questions/Question 3/datacleaning.py
@@ -3,13 +3,18 @@ import numpy as np
 
 # Read the CSV file
 file_path = 'Subgroup-B-datasets/Edsel-data/marketing_campaign_dataset.csv'
-df = pd.read_csv(file_path)
 
-# Display the first few rows of the dataframe
-print(df.head())
 
-df = df.dropna()
-df = df.drop_duplicates()
-df = df.drop('Company', axis = 1) # Drop the 'Company' column
+class DataCleaning:
 
-print(df.head())
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def clean_data(self):
+        df = pd.read_csv(self.file_path)
+        df = df.dropna()
+        df = df.drop_duplicates()
+        df = df.drop('Company', axis = 1) # Drop the 'Company' column
+        return df
+
+print(DataCleaning(file_path).clean_data())
